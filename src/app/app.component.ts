74c9ae32b2e42a16f94cd4b4bdbefe8a6c4dec38
Rory Vidal <rory.vidal@brightsoft.io>
2022-01-10 21:44:24 -0400

    configure and format prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import config from '../../package.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular6';
+  title = config.name + ' ' + config.version;
 }
